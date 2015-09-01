1028704d0543114e8ad11b9a2be7793dec42bddb
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-09-01 00:34:47 +0000

    refatoracao

--- forEach.js
+++ forEach.js
@@ -10,27 +10,27 @@
  */
 this.Ninja.module('$forEach', ['$curry'], function ($curry) {
     
-    /**
-     * Executa uma funcao fornecida uma vez por ordem de elemento
-     * 
-     * @public
-     * @method $forEach
-     * @param {Array} a Array de valores
-     * @param {Function} b Funcao callback que sera executado para cada item do array
-     * @param {Object} c Contexto da funcao callback
-     * @example
-     * 
-     *      $forEach([1, 2, 3], console.log.bind(console));
-     * 
-     */
-    function forEach(a, b, c) {
-        a.forEach(b, c || a);
-    }
+  /**
+   * Executa uma funcao fornecida uma vez por ordem de elemento
+   * 
+   * @public
+   * @method $forEach
+   * @param {Array} a Array de valores
+   * @param {Function} b Funcao callback que sera executado para cada item do array
+   * @param {Object} c Contexto da funcao callback
+   * @example
+   * 
+   *      $forEach([1, 2, 3], console.log.bind(console));
+   * 
+   */
+  function forEach(a, b, c) {
+      a.forEach(b, c || a);
+  }
     
-    /**
+  /**
    * Revelacao do modulo $forEach, encapsulando a visibilidade das funcoes
    * privadas
    */
-    return $curry(forEach);
+  return $curry(forEach);
     
 })
